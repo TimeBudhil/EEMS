c6369bb0692afbc0e6b001131e2c96eedb7eb565
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2026-04-21 12:14:37 -0500

    updated using .env file instead of intellij for db env variables

--- src/main/java/com/mpp/eems/Repository/DBConnection.java
+++ src/main/java/com/mpp/eems/Repository/DBConnection.java
@@ -4,11 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public final class DBConnection {
 
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    static Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     // singleton variable
     private static DBConnection instance;
