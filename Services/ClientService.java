79095913b9a4c09b085407702fb40687e9b73f69
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2026-04-21 10:48:19 -0500

    basic clientservice function

--- Services/ClientService.java
+++ Services/ClientService.java
@@ -1,13 +1,22 @@
 package Services;
 
 import Domain.Client;
+import Domain.Project;
 
 public class ClientService  extends Services{
 
     //lists all clients where their projects have currentDate - project.deadline < daysUntilDeadline
     public static Client[] findClientsByUpcomingProjectDeadline(int daysUntilDeadline){
-
-        return new Client[0];
+        //Get Clients[] using repository
+        Project[] projects = new Project[5];  //TEMPORARY 
+        Client[] clients = new Client[5]; //TEMPORARY WHILE REPO LAYER IS GETTING WORKED ON
+        
+        // return Arrays.stream(projects)
+        //     .filter(p -> LocalDate.now() - p.getDeadline() < daysUntilDeadline)
+        //     .flatMap(p -> p.getClients())
+        //     .distinct()
+        //     .collect(Collectors.toList());
+        return clients;
     }
 
 }
