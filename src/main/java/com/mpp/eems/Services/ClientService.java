f5857098dcc73f3092c5487dad2a5212beb12113
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2026-04-22 12:22:18 -0500

    added basic client crud operations

--- src/main/java/com/mpp/eems/Services/ClientService.java
+++ src/main/java/com/mpp/eems/Services/ClientService.java
@@ -1,12 +1,48 @@
 package com.mpp.eems.Services;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.mpp.eems.Domain.Client;
 import com.mpp.eems.Domain.Project;
+import com.mpp.eems.Repository.ClientRepository;
 
 public class ClientService  extends Services{
+
+    //repo for creating things
+   ClientRepository clientrepo = new ClientRepository();
+
+   /*
+    CRUD OPERAITONS in service
+    
+    */
+    public Client addClient(Client client){
+        Client addedClient = null;
+        //verify client data
+
+        try {
+            addedClient = clientrepo.addClient(client);
+        } catch (SQLException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+
+        return addedClient;
+    }
+
+    public Client findClientByID(int id){
+        return clientrepo.findClient(id);
+    }
+
+    public void updateClient(Client client){
+        clientrepo.modifyClient(client);
+    }
+
+    public void deleteClient(Client client){
+        clientrepo.deleteClient(client.getId());
+    }
+
+
 
     //lists all clients where their projects have currentDate - project.deadline < daysUntilDeadline
     public static List<Client> findClientsByUpcomingProjectDeadline(int daysUntilDeadline){
